b7f579c6ed1f58f51d802b94e26af028e1335e03
Sedláček Jakub <Sedlacek.Jakub@stud.vassboskovice.cz>
2021-11-23 10:34:23 +0100

    Přidejte soubory projektu.

---
+++ statistika/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace statistika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo KeyInfo;
+
+            while (true)
+            {
+
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                Console.WriteLine(keyInfo.Key.ToString());
+            }
+        }
+    }
+}

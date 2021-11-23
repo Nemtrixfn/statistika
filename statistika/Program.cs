0f539380284cd8a1ccde53327e855db5c9ed4b47
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2021-11-23 11:35:44 +0100

    ddd

--- statistika/Program.cs
+++ statistika/Program.cs
@@ -6,15 +6,39 @@ namespace statistika
     {
         static void Main(string[] args)
         {
-            ConsoleKeyInfo KeyInfo;
-
-            while (true)
+            ConsoleKeyInfo keyInfo;
+            string povoleneZnaky = "0123456789";
+            string text = "";
+            int soucet = 0;
+            int pocet = 0;
+            do
             {
+                    keyInfo = Console.ReadKey(true);
+                if (povoleneZnaky.Contains(keyInfo.KeyChar))
+                {
+                    //zde se program dostane při stisku čísla
+                    text += keyInfo.KeyChar; // text = text + keyInfo.KeyChar;
+                    Console.Write(keyInfo.KeyChar);
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    if (text != "")
+                    {
+                        soucet += int.Parse(text);
+                        pocet++; // pocet += 1;
+                    }
+                    Console.WriteLine();
+                    text = "";
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace && text.Length > 0) 
+                                    {
+                    Console.Write("\b \b");
+                    text = text.Substring(0, text.Length - 1);
 
-
-                ConsoleKeyInfo keyInfo = Console.ReadKey();
-                Console.WriteLine(keyInfo.Key.ToString());
-            }
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape);
+            
+           
         }
     }
 }
